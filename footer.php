18b20abdb3a430a1c920f27016c6db7e09bcebd0
Eduardo <e.martinez@nuevaweb.com.mx>
2015-07-31 16:56:44 -0500

    [Main-Nav] Correcciones de padding para el header

--- footer.php
+++ footer.php
@@ -8,10 +8,11 @@
 				<div class="container">
 					<div class="col-sm-12">
 						<?php dynamic_sidebar( 'legales' ); ?>
-						<p>&#169;2015 The Knowledge Applied <br/>Sitio desarrollado por <a href="#">Nueva Web</a></p>
+						<p>&#169;2015 The Knowledge Applied <br/>Sitio desarrollado por <a href="http://www.nuevaweb.com.mx/" target="_blank">Nueva Web</a></p>
 					</div><!--.col-sm-12-->
 				</div><!--.container-->
 			</div><!--.legales-->
 		</footer>
+		<?php wp_footer(); // wordpress admin-bar functions ?>
 </body>
 </html>
